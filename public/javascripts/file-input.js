e86bc13efccfc3a4fca69945317ba240a57b5152
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-05-19 10:56:40 +0900

    アイコンアップロードのビューを作成

---
+++ public/javascripts/file-input.js
@@ -0,0 +1,16 @@
+$("#avatar").fileinput({
+    overwriteInitial: true,
+    maxFileSize: 1500,
+    showClose: false,
+    showCaption: false,
+    browseLabel: '',
+    removeLabel: '',
+    browseIcon: '<i class="glyphicon glyphicon-folder-open"></i>',
+    removeIcon: '<i class="glyphicon glyphicon-remove"></i>',
+    removeTitle: 'Cancel or reset changes',
+    elErrorContainer: '#kv-avatar-errors',
+    msgErrorClass: 'alert alert-block alert-danger',
+    defaultPreviewContent: '<img src="/usericon" alt="Your Avatar" style="width:160px">',
+    layoutTemplates: {main2: '{preview} {upload} {remove} {browse}'},
+    allowedFileExtensions: ["jpg", "png", "gif"]
+});
